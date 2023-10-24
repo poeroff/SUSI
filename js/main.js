06af01246b8992133357790d01504f2bae223668
choi tae young <chlxodud04@naver.com>
2023-10-24 13:54:09 +0900

    ..

--- js/main.js
+++ js/main.js
@@ -1,6 +1,22 @@
 const movieList = document.querySelector(".movie-list");
 const searchBtn = document.getElementById("searchBtn");
 const inputValue = document.getElementById("searchInput");
+const LiveMovie = document.querySelector("Live-Movie")
+const LIVEBUTTON = document.getElementById("Live")
+const COMINGBUTTON = document.getElementById("UPCOMING")
+
+
+
+LIVEBUTTON.addEventListener("click", () => {
+  document.querySelector(".Live-Movie").style.display="flex";
+  document.querySelector(".Upcoming-Movie").style.display="none";
+
+})
+
+COMINGBUTTON.addEventListener("click", () => {
+  document.querySelector(".Live-Movie").style.display="none";
+  document.querySelector(".Upcoming-Movie").style.display="flex";
+})
 
 const options = {
   method: "GET",
@@ -11,35 +27,63 @@ const options = {
   },
 };
 
-let totalArr = [];
+let totalArr = []
 
-const getMovieListFromDB = async () => {
+const getMovieListFromDB = async (url) => {
   try {
-    const res = await fetch(
-      "https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1",
-      options
-    );
+    const res = await fetch(url, options);
     const json = await res.json();
     const { results } = json;
     totalArr = results;
+    return totalArr
   } catch (error) {
     console.error(error);
   }
 };
 
-const paintCard = (backdrop_path, title,  id) => {
-  let movieCard = document.createElement("div");
-  movieCard.classList.add("movie-card");
-  movieCard.dataset.id = id;
-  movieCard.innerHTML = `<img src="https://image.tmdb.org/t/p/w500/${backdrop_path}" alt="${title}">
-  <h3 class="movie-title">${title}</h3>`;
-  movieList.append(movieCard);
+const paintCard = (moviename, backdrop_path, title, id) => {
+  if (moviename === "normal_movie") {
+    let movieCard = document.createElement("div");
+    movieCard.classList.add("movie-card");
+    movieCard.dataset.id = id;
+    movieCard.innerHTML = `<img src="https://image.tmdb.org/t/p/w500/${backdrop_path}" alt="${title}">
+    <h3 class="movie-title">${title}</h3>`;
+    movieList.append(movieCard);
+  }
+  else if (moviename === "Live_movie") {
+
+    let Livemovie = document.createElement("div");
+    Livemovie.classList.add("movie-card");
+    Livemovie.dataset.id = id;
+    Livemovie.innerHTML = `<img src="https://image.tmdb.org/t/p/w500/${backdrop_path}" alt="${title}">
+    <h3 class="movie-title">${title}</h3>`;
+    console.log(Livemovie)
+    document.querySelector(".Live-Movie").append(Livemovie);
+    
+    
+
+
+
+  }
+  else if (moviename === "Upcoming") {
+    let Upcomingmovie = document.createElement("div");
+    Upcomingmovie.classList.add("movie-card");
+    Upcomingmovie.dataset.id = id;
+    Upcomingmovie.innerHTML = `<img src="https://image.tmdb.org/t/p/w500/${backdrop_path}" alt="${title}">
+    <h3 class="movie-title">${title}</h3>`;
+
+    document.querySelector(".Upcoming-Movie").append(Upcomingmovie);
+
+  
+  }
+
+
 };
 
-const paintPage = (arr) => {
+const paintPage = (moviename, arr) => {
   arr.forEach((movie) => {
-    const { backdrop_path, title, overview, vote_average, id } = movie;
-    paintCard(backdrop_path, title, overview, vote_average, id);
+    const { backdrop_path, title, id } = movie;
+    paintCard(moviename, backdrop_path, title, id);
   });
 };
 
@@ -57,10 +101,22 @@ const handleSearchClick = async (event) => {
   }
 };
 
+
+
+const moviearray = ["https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1", 'https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1', 'https://api.themoviedb.org/3/movie/upcoming?language=en-US&page=1']
 const init = async () => {
+  const promises = moviearray.map(async (movie) => {
+    const result = await getMovieListFromDB(movie);
+    return result;
+  });
+
   try {
-    await getMovieListFromDB();
-    paintPage(totalArr);
+    const movieData = await Promise.all(promises);
+    console.log(movieData)
+    paintPage("normal_movie", movieData[0]);
+    paintPage("Live_movie", movieData[1]);
+    paintPage("Upcoming", movieData[2]);
+
   } catch (error) {
     console.error(error);
   }
