19b341f403dc75166fa08c8ad8a0ddad7bcbff45
choi tae young <chlxodud04@naver.com>
2023-10-24 11:02:47 +0900

    card

--- main.js
+++ main.js
@@ -27,17 +27,12 @@ const getMovieListFromDB = async () => {
   }
 };
 
-const paintCard = (backdrop_path, title, overview, vote_average, id) => {
+const paintCard = (backdrop_path, title,  id) => {
   let movieCard = document.createElement("div");
   movieCard.classList.add("movie-card");
   movieCard.dataset.id = id;
-  movieCard.innerHTML = `        <img src="https://image.tmdb.org/t/p/w500/${backdrop_path}" alt="${title}">
-  <h3 class="movie-title">${title}</h3>
-  <p>${overview}</p>
-  <p>Rating:${vote_average}</p>`;
-  movieCard.addEventListener("click", () => {
-    alert(`영화 ID: ${movieCard.dataset.id}`);
-  });
+  movieCard.innerHTML = `<img src="https://image.tmdb.org/t/p/w500/${backdrop_path}" alt="${title}">
+  <h3 class="movie-title">${title}</h3>`;
   movieList.append(movieCard);
 };
 
