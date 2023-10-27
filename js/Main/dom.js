c08c8f6685e99cee0abef845aa9d84c1494ba558
HoyeongJeon <hoyeong0302@gmail.com>
2023-10-27 18:56:41 +0900

    feat: tv, drama 디테일 페이지 구분

--- js/Main/dom.js
+++ js/Main/dom.js
@@ -1,58 +1,76 @@
-export function paintCard (moviename, isvalid, backdrop_path, title, id,name)  {
-  
-    if (moviename === "Top_movie" || moviename === "Top_Tv") {
-      let movieCard = document.createElement("div");
-     
-      movieCard.classList.add("movie-card");
-      movieCard.dataset.id = id;
-      movieCard.dataset.type = isvalid ? "TV" : "MOVIE"
-      
-      movieCard.innerHTML = `<a href="./detail-page.html?id=${id}"><img src="https://image.tmdb.org/t/p/w500/${backdrop_path}" alt="${isvalid ? name : title}">
+export function paintCard(moviename, isvalid, backdrop_path, title, id, name) {
+  if (moviename === "Top_movie" || moviename === "Top_Tv") {
+    let movieCard = document.createElement("div");
+
+    movieCard.classList.add("movie-card");
+    movieCard.dataset.id = id;
+    movieCard.dataset.type = isvalid ? "TV" : "MOVIE"; // isvalid면 TV
+
+    movieCard.innerHTML = `<a href="${
+      isvalid
+        ? "./tv-detail-page.html?type=tv"
+        : "./detail-page.html?type=movie"
+    }&id=${id}"><img src="https://image.tmdb.org/t/p/w500/${backdrop_path}" alt="${
+      isvalid ? name : title
+    }">
       <h3 class="movie-title">${isvalid ? name : title}</h3></a>`;
-      var valid = isvalid ?   ".Tv-list " : ".movie-list";
-      document.querySelector(valid).append(movieCard);
-    }
-    else if (moviename === "Live_movie" || moviename === "Live_Tv") {
-  
-      let Livemovie = document.createElement("div");
-      Livemovie.classList.add("movie-card");
-      Livemovie.dataset.id = id;
-      Livemovie.dataset.type = isvalid ? "TV" : "MOVIE"
-      Livemovie.innerHTML = `<a href="./detail-page.html?id=${id}"><img src="https://image.tmdb.org/t/p/w500/${backdrop_path}" alt="${isvalid ? name : title}">
+    var valid = isvalid ? ".Tv-list " : ".movie-list";
+    document.querySelector(valid).append(movieCard);
+  } else if (moviename === "Live_movie" || moviename === "Live_Tv") {
+    let Livemovie = document.createElement("div");
+    Livemovie.classList.add("movie-card");
+    Livemovie.dataset.id = id;
+    Livemovie.dataset.type = isvalid ? "TV" : "MOVIE";
+    Livemovie.innerHTML = `<a href="${
+      isvalid
+        ? "./tv-detail-page.html?type=tv"
+        : "./detail-page.html?type=movie"
+    }&id=${id}"><img src="https://image.tmdb.org/t/p/w500/${backdrop_path}" alt="${
+      isvalid ? name : title
+    }">
       <h3 class="movie-title">${isvalid ? name : title}</h3></a>`;
-  
-      let valid = isvalid ? ".Live_Tv-list" : ".Live-Movie";
-      document.querySelector(valid).append(Livemovie);
-    }
-    else if (moviename === "Upcoming" || moviename === "Upcoming_Tv") {
-      let Upcomingmovie = document.createElement("div");
-      Upcomingmovie.classList.add("movie-card");
-      Upcomingmovie.dataset.id = id;
-      Upcomingmovie.dataset.type = isvalid ? "TV" : "MOVIE"
-      
-      Upcomingmovie.innerHTML = `<a href="./detail-page.html?id=${id}"><img src="https://image.tmdb.org/t/p/w500/${backdrop_path}" alt="${isvalid ? name : title}">
+
+    let valid = isvalid ? ".Live_Tv-list" : ".Live-Movie";
+    document.querySelector(valid).append(Livemovie);
+  } else if (moviename === "Upcoming" || moviename === "Upcoming_Tv") {
+    let Upcomingmovie = document.createElement("div");
+    Upcomingmovie.classList.add("movie-card");
+    Upcomingmovie.dataset.id = id;
+    Upcomingmovie.dataset.type = isvalid ? "TV" : "MOVIE";
+
+    Upcomingmovie.innerHTML = `<a href="${
+      isvalid
+        ? "./tv-detail-page.html?type=tv"
+        : "./detail-page.html?type=movie"
+    }&id=${id}"><img src="https://image.tmdb.org/t/p/w500/${backdrop_path}" alt="${
+      isvalid ? name : title
+    }">
       <h3 class="movie-title">${isvalid ? name : title}</h3></a>`;
-      let valid = isvalid ? ".Upcoming_Tv-list" :".Upcoming-Movie"
-     
-      document.querySelector(valid).append(Upcomingmovie);
-    }
-    else if (moviename === "Popular_moive" || moviename === "popular_Tv") {
-    
-      let Popularmovie = document.createElement("div");
-      Popularmovie.classList.add("movie-card");
-      Popularmovie.dataset.id = id;
-      Popularmovie.dataset.type = isvalid ? "TV" : "MOVIE"
-      Popularmovie.innerHTML = `<a href="./detail-page.html?id=${id}"><img src="https://image.tmdb.org/t/p/w500/${backdrop_path}" alt="${isvalid ? name : title}">
+    let valid = isvalid ? ".Upcoming_Tv-list" : ".Upcoming-Movie";
+
+    document.querySelector(valid).append(Upcomingmovie);
+  } else if (moviename === "Popular_moive" || moviename === "popular_Tv") {
+    let Popularmovie = document.createElement("div");
+    Popularmovie.classList.add("movie-card");
+    Popularmovie.dataset.id = id;
+    Popularmovie.dataset.type = isvalid ? "TV" : "MOVIE";
+    Popularmovie.innerHTML = `<a href="${
+      isvalid
+        ? "./tv-detail-page.html?type=tv"
+        : "./detail-page.html?type=movie"
+    }&id=${id}"><img src="https://image.tmdb.org/t/p/w500/${backdrop_path}" alt="${
+      isvalid ? name : title
+    }">
       <h3 class="movie-title">${isvalid ? name : title}</h3></a>`;
-      let valid = isvalid ? ".popular_Tv_list" : ".popular_movie";
-     
-      document.querySelector(valid).append(Popularmovie);
-    }
-  };
-  
-  export function paintPage( moviename,isvalid, arr) {
-    arr.forEach((movie) => {
-      const { backdrop_path, title, id ,name} = movie;
-      paintCard(moviename, isvalid, backdrop_path, title, id,name);
-    });
-  };
+    let valid = isvalid ? ".popular_Tv_list" : ".popular_movie";
+
+    document.querySelector(valid).append(Popularmovie);
+  }
+}
+
+export function paintPage(moviename, isvalid, arr) {
+  arr.forEach((movie) => {
+    const { backdrop_path, title, id, name } = movie;
+    paintCard(moviename, isvalid, backdrop_path, title, id, name);
+  });
+}
--- js/tv-detail-page.js
+++ js/tv-detail-page.js
@@ -7,187 +7,148 @@ const p0 = document.querySelector(".p0");
 
 // fetch 옵션
 let tvDetailInfo = async (tv_id) => {
-    const tvActorUrl = `https://api.themoviedb.org/3/tv/${tv_id}`;
-    const options = {
-        method: "GET",
-        headers: {
-            accept: "application/json",
-            Authorization:
-                "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhMDkzNzRjY2EyNTc1NjM4ZDEwMDk3NzAzYjFhODliYSIsInN1YiI6IjY1MmZiZWRkYTgwMjM2MDBmZDJkOWY0NiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ga70Ew8jOvgDuOUzMiuJgfI8GjGGypablmY74WjMtUs",
-        },
-    };
-    const res = await fetch(tvActorUrl, options);
-    const data = await res.json();
-    if (res.ok) {
-        return data;
-    } else {
-        throw Error(data);
-    }
+  const tvActorUrl = `https://api.themoviedb.org/3/tv/${tv_id}`;
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhMDkzNzRjY2EyNTc1NjM4ZDEwMDk3NzAzYjFhODliYSIsInN1YiI6IjY1MmZiZWRkYTgwMjM2MDBmZDJkOWY0NiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ga70Ew8jOvgDuOUzMiuJgfI8GjGGypablmY74WjMtUs",
+    },
+  };
+  const res = await fetch(tvActorUrl, options);
+  const data = await res.json();
+  if (res.ok) {
+    return data;
+  } else {
+    throw Error(data);
+  }
 };
 const drawTvInfo = async () => {
-    try {
-        // const urlParams = new URLSearchParams(window.location.search);
-        // let movieId = urlParams.get("id");
-        // movieInfoArr(movieId);
-        const totallActorFunc = await tvDetailInfo(9477);
-        console.log(totallActorFunc);
-        const tvActorInfoPath = totallActorFunc.poster_path;
-        const tvActorInfoName = totallActorFunc.name;
-        const tvActorInfoOverview = totallActorFunc.overview;
-        const tvActorInfoVote = totallActorFunc.vote_average;
-        // ===============================
-        const tvTitleImage = document.querySelector(".imageTv");
-        const tvTitleH = document.querySelector(".tvTitle");
-        const tvTitleP = document.querySelector(".tvOverView");
-        tvTitleH.innerText = tvActorInfoName;
-        tvTitleP.innerText = tvActorInfoOverview;
-        tvTitleImage.setAttribute("src", `https://image.tmdb.org/t/p/w500/${tvActorInfoPath}`);
-        throw erro;
-    } catch {
-        image.setAttribute(
-            "src",
-            `https://search.pstatic.net/sunny/?src=https%3A%2F%2Fi.pinimg.com%2F736x%2Ff3%2Ffc%2Ffe%2Ff3fcfe3bb9539323c8d62aa105b15563.jpg&type=a340`
-        );
-        p0.innerHTML = "The information could not be retrieved.";
-    }
+  try {
+    const urlParams = new URLSearchParams(window.location.search);
+    let tvId = urlParams.get("id");
+    const totallActorFunc = await tvDetailInfo(tvId);
+    console.log(totallActorFunc);
+    const tvActorInfoPath = totallActorFunc.poster_path;
+    const tvActorInfoName = totallActorFunc.name;
+    const tvActorInfoOverview = totallActorFunc.overview;
+    const tvActorInfoVote = totallActorFunc.vote_average;
+    // ===============================
+    const tvTitleImage = document.querySelector(".imageTv");
+    const tvTitleH = document.querySelector(".tvTitle");
+    const tvTitleP = document.querySelector(".tvOverView");
+    tvTitleH.innerText = tvActorInfoName;
+    tvTitleP.innerText = tvActorInfoOverview;
+    tvTitleImage.setAttribute(
+      "src",
+      `https://image.tmdb.org/t/p/w500/${tvActorInfoPath}`
+    );
+    throw erro;
+  } catch {
+    image.setAttribute(
+      "src",
+      `https://search.pstatic.net/sunny/?src=https%3A%2F%2Fi.pinimg.com%2F736x%2Ff3%2Ffc%2Ffe%2Ff3fcfe3bb9539323c8d62aa105b15563.jpg&type=a340`
+    );
+    p0.innerHTML = "The information could not be retrieved.";
+  }
 };
 
 drawTvInfo();
 
-// const paintTvInfo = (tvInfo) => {
-//     // 이미지와 기타 영화 정보 웹페이지에 출력
-//     console.log(tvInfo);
-//     // 이미지가 존재할 때
-//     if (tvInfo[0] !== undefined) {
-//         image.setAttribute("src", `https://image.tmdb.org/t/p/w500/${tvInfo[0]}`);
-//         p1.innerHTML = `${tvInfo[1]}`;
-//         // p2.innerHTML = `평점 : ${tvInfo[3]}`;
-//         p3.innerHTML = ` ${tvInfo[2]}`;
-
-//         // 이미지가 존재하지 않을 때
-//     } else if (tvInfo[0] === undefined) {
-//         image.setAttribute(
-//             "src",
-//             `https://search.pstatic.net/sunny/?src=https%3A%2F%2Fi.pinimg.com%2F736x%2Ff3%2Ffc%2Ffe%2Ff3fcfe3bb9539323c8d62aa105b15563.jpg&type=a340`
-//         );
-//         p0.innerHTML = "The information could not be retrieved.";
-//     }
-// };
-// -------------------------------------------------------------------
-// -------------------------------------------------------------------
-// -------------------------------------------------------------------
-// -------------------------------------------------------------------
-
-// const post = async (movie_id) => {
-//     const url = `https://api.themoviedb.org/3/movie/${movie_id}/videos`;
-//     const options = {
-//         method: "GET",
-//         headers: {
-//             accept: "application/json",
-//             Authorization:
-//                 "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhMDkzNzRjY2EyNTc1NjM4ZDEwMDk3NzAzYjFhODliYSIsInN1YiI6IjY1MmZiZWRkYTgwMjM2MDBmZDJkOWY0NiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ga70Ew8jOvgDuOUzMiuJgfI8GjGGypablmY74WjMtUs",
-//         },
-//     };
-//     const res = await fetch(url, options);
-//     const data = await res.json();
-//     if (res.ok) {
-//         return data;
-//     } else {
-//         throw Error(data);
-//     }
-// };
-
 const init = async () => {
-    try {
-        const urlParams = new URLSearchParams(window.location.search);
-        let movieId = urlParams.get("id");
-        movieInfoArr(movieId);
-        let totallFunc = await post(movieId);
-        let totallArr = Array.from(totallFunc.results);
-        let filteredArr = totallArr.filter((movie) => {
-            return movie.type === "Trailer";
-        });
-        let filteredArrMinus = filteredArr.slice(0, 1);
-        filteredArrMinus.forEach((video) => {
-            let videoUrl = `https://www.youtube.com/embed/${video.key}`;
-            const videoElm = document.querySelector(".videoCss");
-            const videoIframe = document.createElement("iframe");
-            videoIframe.className = "video";
-            videoIframe.setAttribute("width", 2000);
-            videoIframe.setAttribute("height", 600);
-            videoIframe.setAttribute("margin", "600px");
-            videoIframe.setAttribute("allow", "fullscreen");
-            videoIframe.setAttribute("src", videoUrl);
-            videoElm.append(videoIframe);
-        });
-        throw error;
-    } catch (e) {
-        const errE = document.querySelector(".videoCss");
-        const errIcon = document.createElement("i");
-        errIcon.className = "fas fa-video-slash fa-4x";
-        errE.append(errIcon);
-    }
+  try {
+    const urlParams = new URLSearchParams(window.location.search);
+    let movieId = urlParams.get("id");
+    movieInfoArr(movieId);
+    let totallFunc = await post(movieId);
+    let totallArr = Array.from(totallFunc.results);
+    let filteredArr = totallArr.filter((movie) => {
+      return movie.type === "Trailer";
+    });
+    let filteredArrMinus = filteredArr.slice(0, 1);
+    filteredArrMinus.forEach((video) => {
+      let videoUrl = `https://www.youtube.com/embed/${video.key}`;
+      const videoElm = document.querySelector(".videoCss");
+      const videoIframe = document.createElement("iframe");
+      videoIframe.className = "video";
+      videoIframe.setAttribute("width", 2000);
+      videoIframe.setAttribute("height", 600);
+      videoIframe.setAttribute("margin", "600px");
+      videoIframe.setAttribute("allow", "fullscreen");
+      videoIframe.setAttribute("src", videoUrl);
+      videoElm.append(videoIframe);
+    });
+    throw error;
+  } catch (e) {
+    const errE = document.querySelector(".videoCss");
+    const errIcon = document.createElement("i");
+    errIcon.className = "fas fa-video-slash fa-4x";
+    errE.append(errIcon);
+  }
 };
 
 init();
 
 let tvActor = async (movie_id) => {
-    const tvActorUrl = `https://api.themoviedb.org/3/tv/${movie_id}/credits?language=en-US`;
-    const options = {
-        method: "GET",
-        headers: {
-            accept: "application/json",
-            Authorization:
-                "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhMDkzNzRjY2EyNTc1NjM4ZDEwMDk3NzAzYjFhODliYSIsInN1YiI6IjY1MmZiZWRkYTgwMjM2MDBmZDJkOWY0NiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ga70Ew8jOvgDuOUzMiuJgfI8GjGGypablmY74WjMtUs",
-        },
-    };
-    const res = await fetch(tvActorUrl, options);
-    const data = await res.json();
-    console.log(data);
-    if (res.ok) {
-        return data;
-    } else {
-        throw Error(data);
-    }
+  const tvActorUrl = `https://api.themoviedb.org/3/tv/${movie_id}/credits?language=en-US`;
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhMDkzNzRjY2EyNTc1NjM4ZDEwMDk3NzAzYjFhODliYSIsInN1YiI6IjY1MmZiZWRkYTgwMjM2MDBmZDJkOWY0NiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ga70Ew8jOvgDuOUzMiuJgfI8GjGGypablmY74WjMtUs",
+    },
+  };
+  const res = await fetch(tvActorUrl, options);
+  const data = await res.json();
+  console.log(data);
+  if (res.ok) {
+    return data;
+  } else {
+    throw Error(data);
+  }
 };
 const drawActorInfo = async () => {
-    // const urlParams = new URLSearchParams(window.location.search);
-    // let movieId = urlParams.get("id");
-    // movieInfoArr(movieId);
-    let totallActorFunc = await tvActor(9477);
-    let totallActorArr = Array.from(totallActorFunc.cast);
-    console.log(totallActorArr);
-    totallActorArr.splice(10);
-    totallActorArr.map((actor) => {
-        const actorUrl = "https://image.tmdb.org/t/p/w200";
-        let actorPath = actor.profile_path;
-        let actorName = actor.name;
-        let actorUrlPath = actorPath ? actorUrl + actorPath : "./assets/imgs/noimage.jpeg";
-        let actorCharacter = actor.character;
-        // =============================================
-        const actorCard = document.querySelector(".actorView");
-        const actorPotoImg = document.createElement("img");
-        const actorPotoP = document.createElement("p");
-        const actorPotoCharacter = document.createElement("p");
-        const actorCharacterTitle = document.createElement("p");
-        const actorPotoDiv = document.createElement("div");
-        const errImgActor = "NO ACTOR IMG";
-        actorPotoP.innerText = actorName;
-        actorCharacterTitle.innerText = "Character:";
-        actorPotoCharacter.innerText = actorCharacter;
-        actorPotoDiv.className = "basket";
-        actorPotoP.className = "actorRealNameWho";
-        actorCharacterTitle.className = "actorWho";
-        actorPotoCharacter.className = "actorWho";
-        actorPotoImg.className = "actorPic";
-        actorPotoImg.setAttribute("src", actorUrlPath);
-        actorPotoImg.setAttribute("alt", errImgActor);
-        actorPotoDiv.append(actorPotoCharacter);
-        actorPotoDiv.prepend(actorCharacterTitle);
-        actorPotoDiv.prepend(actorPotoP);
-        actorPotoDiv.prepend(actorPotoImg);
-        actorCard.append(actorPotoDiv);
-    });
+  const urlParams = new URLSearchParams(window.location.search);
+  let tvId = urlParams.get("id");
+  //   movieInfoArr(movieId);
+  let totallActorFunc = await tvActor(tvId);
+  let totallActorArr = Array.from(totallActorFunc.cast);
+  console.log(totallActorArr);
+  totallActorArr.splice(10);
+  totallActorArr.map((actor) => {
+    const actorUrl = "https://image.tmdb.org/t/p/w200";
+    let actorPath = actor.profile_path;
+    let actorName = actor.name;
+    let actorUrlPath = actorPath
+      ? actorUrl + actorPath
+      : "./assets/imgs/noimage.jpeg";
+    let actorCharacter = actor.character;
+    // =============================================
+    const actorCard = document.querySelector(".actorView");
+    const actorPotoImg = document.createElement("img");
+    const actorPotoP = document.createElement("p");
+    const actorPotoCharacter = document.createElement("p");
+    const actorCharacterTitle = document.createElement("p");
+    const actorPotoDiv = document.createElement("div");
+    const errImgActor = "NO ACTOR IMG";
+    actorPotoP.innerText = actorName;
+    actorCharacterTitle.innerText = "Character:";
+    actorPotoCharacter.innerText = actorCharacter;
+    actorPotoDiv.className = "basket";
+    actorPotoP.className = "actorRealNameWho";
+    actorCharacterTitle.className = "actorWho";
+    actorPotoCharacter.className = "actorWho";
+    actorPotoImg.className = "actorPic";
+    actorPotoImg.setAttribute("src", actorUrlPath);
+    actorPotoImg.setAttribute("alt", errImgActor);
+    actorPotoDiv.append(actorPotoCharacter);
+    actorPotoDiv.prepend(actorCharacterTitle);
+    actorPotoDiv.prepend(actorPotoP);
+    actorPotoDiv.prepend(actorPotoImg);
+    actorCard.append(actorPotoDiv);
+  });
 };
 
 drawActorInfo();
